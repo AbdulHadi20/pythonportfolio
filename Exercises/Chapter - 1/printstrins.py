c505238f921bbd8f0b314474997b260fe3a36060
AbdulHadi20 <104209042+AbdulHadi20@users.noreply.github.com>
2022-10-09 17:09:25 +0400

    Add files via upload

---
+++ Exercises/Chapter - 1/printstrins.py
@@ -0,0 +1,6 @@
+# program to print favorite rhymes using print() function
+
+print('Twinkle twinkle little star')
+print('How I wonder what you are')
+print('Up about the world so high')
+print('Like a diamond in the sky')
