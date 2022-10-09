a9aa26056415b95ec0a8af2b77f3b8dea455db9c
AbdulHadi20 <104209042+AbdulHadi20@users.noreply.github.com>
2022-10-09 17:15:49 +0400

    String Concatination
    Exercise - 4

---
+++ Exercises/Chapter - 1/stringconcatination.py
@@ -0,0 +1,8 @@
+# a program to store strings in different variables and
+# print using a single print(function)
+
+string_1 = 'My name is AbdulHadi.'
+string_2 = 'I am 19 years old.'
+string_3 = 'I am from Pakistan.'
+
+print(string_1 + '\n' + string_2 + '\n' + string_3)
