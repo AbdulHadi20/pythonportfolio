d118d3630efc196402d8fca5975485e7320b474a
AbdulHadi20 <104209042+AbdulHadi20@users.noreply.github.com>
2022-10-09 17:12:23 +0400

    Python Version Check
    Exercise - 2

---
+++ Exercises/Chapter - 1/pythonversioncheck.py
@@ -0,0 +1,9 @@
+# program to find the python version
+
+import sys  # the sys is a library called to verify the function
+
+print('Python Version')  # this part displays the shortened information
+print(sys.version)
+
+print('Version Information')  # this part displays the complete details
+print(sys.version_info)
