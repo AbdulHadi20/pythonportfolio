c1944eebb9b3a610d3e1f7b386a31d7772545f7f
AbdulHadi20 <104209042+AbdulHadi20@users.noreply.github.com>
2022-10-13 10:35:43 +0400

    Add files via upload

---
+++ Exercises/Chapter-2/quotefromfavperson.py
@@ -0,0 +1,5 @@
+# a program to write a quote from your favorite person
+
+quote = '"The best time to plant a tree was yesterday, the second best time is now."'
+
+print('Ali Abdaal once quoted a famous proverb. He said, ' + str(quote))
