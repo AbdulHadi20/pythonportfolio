70f8bdc2cbe573c7ec14414c8b7ce63061873259
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-10-18 17:28:32 +0400

    Add files via upload

---
+++ Python - Exercise/Chapter-3/chap-3ex-5.py
@@ -0,0 +1,16 @@
+# a program to modify guest list
+
+guest_list = ['Joe', 'Kristian', 'Ryan', 'Rafael']
+
+print('Just heard that ' + guest_list[2] + " wouldn't be able to make it....")
+
+guest_list[2] = 'Patrick'
+print(guest_list)
+
+print('Hi again ' + guest_list[0] +
+      ', You are invited for dinner at my house. ')
+print('Hi again ' + guest_list[1] +
+      ', You are invited for dinner at my house. ')
+print('Hi  ' + guest_list[2] + ', You are invited for dinner at my house. ')
+print('Hi again ' + guest_list[3] +
+      ', You are invited for dinner at my house. ')
