280fd13c31091057cbd83888e3f847c555752bc8
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-10-18 17:28:14 +0400

    Delete Chapter-3 directory

--- Chapter-3/chap-3ex-1.py
+++
@@ -1,9 +0,0 @@
-# a program to save names in a list and printing one by one
-
-names = ['Shaheer', 'Shehroz', 'Zoraiz', 'Maahin', 'Ali']
-
-print(names[0])
-print(names[1])
-print(names[2])
-print(names[3])
-print(names[4])
