9bada56bac5dde1fba8b56dd52f91f0b840abff0
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-10-20 19:17:11 +0400

    Update chap-4ex-2.py

--- Python - Exercise/Chapter-4/chap-4ex-2.py
+++ Python - Exercise/Chapter-4/chap-4ex-2.py
@@ -1,9 +1,9 @@
 # a program to demonstrate the working of if-else statements
 
 # in this section, the if statement is true
-alien_color = 'white'
+alien_color = 'green'
 
-if alien_color == 'white':
+if alien_color == 'green':
     print('Congratulations, you have earned 5 points.')
 
 else:
@@ -12,7 +12,7 @@ else:
 # in this section, the else statement is true
 alien_color = 'pink'
 
-if alien_color == 'white':
+if alien_color == 'green':
     print('Congratulations, you have earned 5 points.')
 
 else:
