0c4a21db9e2f28483a4382dbc062c37f0f489155
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-10-26 12:18:11 +0400

    Add files via upload

---
+++ Python - Exercise/Chapter-5/chap-5ex-1.py
@@ -0,0 +1,9 @@
+# a program to use a dictionary to store information of a person
+
+user_info = {'first_name': 'AbdulHadi',
+             'last_name': 'Aamir', 'city': 'Dubai', 'age': '19'}
+
+print(user_info['first_name'])
+print(user_info['last_name'])
+print(user_info['city'])
+print(user_info['age'])
---
+++ Python - Exercise/Chapter-5/chap-5ex-2.py
@@ -0,0 +1,30 @@
+# a program to make glossary
+
+glossary = {'loop':
+            'a loop is an operation in programming that repeats a certain statement until a certain condition is met',
+            'concatenation':
+            'it refers to the joining of multiple strings to make a single long string.',
+            'bin()':
+            'it is the function, used to find the binary value of a certain character.',
+            'IDE':
+            'IDE stands for integrated development environment. It is a space built for coders to run, save, debug, etc. their code in a single environment. ',
+            'GitHub': 'It is a platform where we can store our code projects in a cloud based repositories, collaborate with other developers and control the versions.'}
+
+print('Loop:')
+print(glossary['loop'])
+print('\n')
+
+print('Concatenation:')
+print(glossary['concatenation'])
+print('\n')
+
+print('bin():')
+print(glossary['bin()'])
+print('\n')
+
+print('IDE:')
+print(glossary['IDE'])
+print('\n')
+
+print('GitHub:')
+print(glossary['GitHub'])
