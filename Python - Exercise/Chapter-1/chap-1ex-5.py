4e1d12357c8c9a9bdc6efea5de693ef09c101c66
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-01 23:36:14 +0400

    Update chap-1ex-5.py

--- Python - Exercise/Chapter-1/chap-1ex-5.py
+++ Python - Exercise/Chapter-1/chap-1ex-5.py
@@ -1,10 +1,15 @@
-# a program to find the area of the circle
+"""
+Chapter 1, Exercise 5: Compute area of circle
 
-print('Enter the radius of the circle = ')
+Write a Python program which accepts the radius of a circle from the user and compute the area.
+"""
+
+print('Enter the radius of the circle = ')  # takes value from the user
 radius = float(input())
 
-pi = 3.14
-area = pi * radius * radius
+pi = 3.14                                                                 
+area = pi * radius * radius  # calcultaion
 
+#output statement
 print('The area of the circle with radius ' +
       str(radius) + 'units is ' + str(area) + 'sq.units')
