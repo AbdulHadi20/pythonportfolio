78301a92b61a776c9090e20f3b229ccf225f26a2
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-01 23:25:24 +0400

    Update chap-1ex-1.py

--- Python - Exercise/Chapter-1/chap-1ex-1.py
+++ Python - Exercise/Chapter-1/chap-1ex-1.py
@@ -1,7 +1,15 @@
-# writing my favorite English rhymes
-# using print() statements for every line, the text between the single/double quotes
-# is displayed as typed.
+"""
+Chapter 1, Exercise 1: Print Strings
 
+Write a Python program to print the following string in a specific format.
+Twinkle twinkle little star
+How I wonder shat you are
+Up about the world so high
+Like a diamond in the sky
+Twinkle twinkle little star
+How I wonder what you are
+"""
+#using multiple print() statements
 print('Twinkle twinkle little star')
 print('How I wonder what you are')
 print('Up about the world so high')
