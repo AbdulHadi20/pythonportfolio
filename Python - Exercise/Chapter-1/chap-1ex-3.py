f5a955641f84a750195d69d7572eead6c434ef98
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-01 23:30:33 +0400

    Update chap-1ex-3.py

--- Python - Exercise/Chapter-1/chap-1ex-3.py
+++ Python - Exercise/Chapter-1/chap-1ex-3.py
@@ -1,8 +1,14 @@
+"""
+Chapter 1, Exercise 3: Print date and time
+
+Write a Python program to display the current date and time.
+ 
+"""
 # a program to display current date and time
 
 import time  # importing time module
 from datetime import date  # from the datetime library, importing date module
-from datetime import datetime
+from datetime import datetime # from datetime library, importing datetime module
 
 date = date.today()
 print("Today's date is ", date)
@@ -10,7 +16,7 @@ print("Today's date is ", date)
 print("\n")
 
 t = time.localtime()
-current_time = time.strftime("%H : %M : %S", t)
+current_time = time.strftime("%H : %M : %S", t)  # date format has been defined between the brackets
 print('The current time is ', current_time)
 
 print("\n")
