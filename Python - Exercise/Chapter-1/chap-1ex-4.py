28c3ee1be785fcaf7cca7317274fcf73845e88ac
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-01 23:33:33 +0400

    Update chap-1ex-4.py

--- Python - Exercise/Chapter-1/chap-1ex-4.py
+++ Python - Exercise/Chapter-1/chap-1ex-4.py
@@ -1,8 +1,11 @@
-# a program to store strings in different variables and
-# print using a single print(function)
+"""
+Chapter 1, Exercise 4: Concatenating Strings
 
+Write three strings in different variables and print the output as one string.      
+"""
+# making different variables for strings
 string_1 = 'My name is AbdulHadi.'
 string_2 = 'I am 19 years old.'
 string_3 = 'I am from Pakistan.'
 
-print(string_1 + '\n' + string_2 + '\n' + string_3)
+print(string_1 + '\n' + string_2 + '\n' + string_3) # printing in a single statement
