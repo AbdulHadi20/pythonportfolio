c18dc89366fc484b73bc48f1bcd7b2c48ac9a476
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 01:22:48 +0400

    Update chap-3ex-4.py

--- Python - Exercise/Chapter-3/chap-3ex-4.py
+++ Python - Exercise/Chapter-3/chap-3ex-4.py
@@ -1,8 +1,13 @@
-# a program to make a list of guests to be invited & send a personalized msg
+"""
+Chapter 3, Exercise 4: Guest Lsit
 
-guest_list = ['Joe', 'Kristian', 'Ryan', 'Rafael']
+If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
+Then use your list to print a message to each person, invitingthem to dinner.
+"""
 
-print('Hi ' + guest_list[0] +
+guest_list = ['Joe', 'Kristian', 'Ryan', 'Rafael'] # list of guests
+
+print('Hi ' + guest_list[0] +                                                  # prints the msg seperately for each guest
       ', I would like to invite you for dinner at my house tomorrow.')
 print('Hi ' + guest_list[1] +
       ', I would like to invite you for dinner at my house tomorrow.')
