8c083a53f14d9d21fec4dec7f6510fdb5f0170e2
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 01:26:40 +0400

    Update chap-3ex-5.py

--- Python - Exercise/Chapter-3/chap-3ex-5.py
+++ Python - Exercise/Chapter-3/chap-3ex-5.py
@@ -1,13 +1,20 @@
-# a program to modify guest list
+"""
+Chapter 3, Exercise 5: Change guest list
 
-guest_list = ['Joe', 'Kristian', 'Ryan', 'Rafael']
+You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+•Start with your program from Exercise 3-4. Add a print() call at the end of your program stating the name of the guest who can’t make it.
+•Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+•Print a second set of invitation messages, one for each person who is still in your list.
+"""
 
-print('Just heard that ' + guest_list[2] + " wouldn't be able to make it....")
+guest_list = ['Joe', 'Kristian', 'Ryan', 'Rafael']        # list of guests
 
-guest_list[2] = 'Patrick'
+print('Just heard that ' + guest_list[2] + " wouldn't be able to make it....")   # msg that a guest won't make it 
+
+guest_list[2] = 'Patrick'  # replacing a guest
 print(guest_list)
 
-print('Hi again ' + guest_list[0] +
+print('Hi again ' + guest_list[0] +                                              # sending msgs to all the guests in the modified list
       ', You are invited for dinner at my house. ')
 print('Hi again ' + guest_list[1] +
       ', You are invited for dinner at my house. ')
