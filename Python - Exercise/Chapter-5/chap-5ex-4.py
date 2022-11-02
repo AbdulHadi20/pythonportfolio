b0aeaf1c8f0394a636904b4fad5ba22500274f04
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 02:07:23 +0400

    Update chap-5ex-4.py

--- Python - Exercise/Chapter-5/chap-5ex-4.py
+++ Python - Exercise/Chapter-5/chap-5ex-4.py
@@ -1,4 +1,11 @@
-# a program to make a dictionary of 3 major rivers, print key-value seperately too
+"""
+Chapter 5, Exercise 4: Rivers
+
+Make a dictionary containing three major rivers and the country each river runs through. One key-value pair might be 'nile': 'egypt'.
+Use a loop to print a sentence about each river, such as The Nile runs through Egypt.
+Use a loop to print the name of each river included in the dictionary.
+Use a loop to print the name of each country included in the dictionary.
+"""
 
 rivers = {
     'Nile': 'Egypt',
