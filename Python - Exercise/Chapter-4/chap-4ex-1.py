47865e9b191c0f1cddd3d377fa41a2b1466d30f1
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 01:40:04 +0400

    Update chap-4ex-1.py

--- Python - Exercise/Chapter-4/chap-4ex-1.py
+++ Python - Exercise/Chapter-4/chap-4ex-1.py
@@ -1,4 +1,10 @@
-# a program to tell wether the alien color is green or not (true)
+"""
+Chapter 4, Exercise 1: Alien Colors #1
+
+Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+•Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+•Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+"""
 
 # in this section, the condition is true
 alien_color = 'green'
