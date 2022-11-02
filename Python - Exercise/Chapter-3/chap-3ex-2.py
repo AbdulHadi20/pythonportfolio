f8fb762dd607f8c4823c84f214fc9b5ce4b29ceb
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 01:17:05 +0400

    Update chap-3ex-2.py

--- Python - Exercise/Chapter-3/chap-3ex-2.py
+++ Python - Exercise/Chapter-3/chap-3ex-2.py
@@ -1,8 +1,13 @@
-# a program to write personalized messages for names in lists
+"""
+Chapter 3, Exercise 2: Greetings
 
-names = ['Shaheer', 'Shehroz', 'Zoraiz', 'Maahin', 'Ali']
+Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them.
+The text of each message should be the same, but each message should be personalized with the person’s name.
+"""
 
-print('Hello ' + names[0] + ', Please let me know when you leave for home.')
+names = ['Shaheer', 'Shehroz', 'Zoraiz', 'Maahin', 'Ali']   # list of names
+
+print('Hello ' + names[0] + ', Please let me know when you leave for home.') # calling every item specifically and printing a personalized msg
 print('Hello ' + names[1] + ', Lets have tea after 9:00 pm.')
 print('Hello ' + names[2] + ', How are you doing?')
 print('Hello ' + names[3] + ', Have a safe flight to Thailand.')
