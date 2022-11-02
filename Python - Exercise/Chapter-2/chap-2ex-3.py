8821d24fdf8e2f01c23d6fd808063912146523b9
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 00:56:47 +0400

    Update chap-2ex-3.py

--- Python - Exercise/Chapter-2/chap-2ex-3.py
+++ Python - Exercise/Chapter-2/chap-2ex-3.py
@@ -1,4 +1,13 @@
-# a program to display the use of lstrip(), rstrip(), strip(), \n, \t
+"""
+Chapter 2, Exercise 3: Stripping names
+
+Tidy up the code to make it easier to understand
+Use a variable to represent a person’s name, and include some whitespace characters at the beginning and end of the name. 
+Make sure you use each character combination, “\t” and “\n”, at least once.
+
+Print the name once, so the whitespace around the name is displayed.
+Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().
+"""
 
 # use of lstrip()
 
