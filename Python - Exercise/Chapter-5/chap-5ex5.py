e6e6fe496e876691f2d76632a01c1cecb194af08
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 02:11:07 +0400

    Update chap-5ex5.py

--- Python - Exercise/Chapter-5/chap-5ex5.py
+++ Python - Exercise/Chapter-5/chap-5ex5.py
@@ -1,5 +1,12 @@
-# making several dictionaries, storing them in a list, then printing using loops
+"""
+Chapter 5, Exercise 5: Pets
 
+
+Make several dictionaries, where each dictionary represents a different pet. In each dictionary, include the kind of animal and the
+owner’s name. Store these dictionaries in a list called pets. Next, loop through your list and asyou do, print everything you know about
+each pet.
+   
+"""
 pet_1 = {
     'animal': 'cat',
     'owner': 'AbdulHadi',
