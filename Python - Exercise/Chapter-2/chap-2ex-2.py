8b82190056cc55100325a22f0059e39db1a783c1
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 00:54:19 +0400

    Update chap-2ex-2.py

--- Python - Exercise/Chapter-2/chap-2ex-2.py
+++ Python - Exercise/Chapter-2/chap-2ex-2.py
@@ -1,4 +1,10 @@
-# a program to write a quote from your favorite person
+"""
+Chapter 2, Exercise 2: Variables
+
+Find a quote from a famous person you admire. Print the quote and the name of its author.
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.
+"""
 
 quote = '"The best time to plant a tree was yesterday, the second best time is now."'
 
