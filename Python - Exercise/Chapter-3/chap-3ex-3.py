831feec4e6eebce3123dc8b15bacc85977061aa4
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 01:20:22 +0400

    Update chap-3ex-3.py

--- Python - Exercise/Chapter-3/chap-3ex-3.py
+++ Python - Exercise/Chapter-3/chap-3ex-3.py
@@ -1,8 +1,13 @@
-# a program to work with lists
+"""
+Chapter 3, Exercise 3: Your Own List
 
-transport_modes = ['car', 'bus', 'metro', 'boat']
+Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+"""
 
-print('Travelling by ' + transport_modes[0].title() + ' is really easy.')
+transport_modes = ['car', 'bus', 'metro', 'boat'] # list of transportations
+
+print('Travelling by ' + transport_modes[0].title() + ' is really easy.')  # printing each means of transport with a specific msg
 print('I travel to the university by ' +
       transport_modes[1].title() + ' everyday.')
 print('My favorite means of transportation is travelling by ' +
