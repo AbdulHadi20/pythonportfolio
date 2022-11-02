b8c4028adc2eee6306aac6bb723d716bfe265f30
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 01:50:43 +0400

    Update chap-4ex-5.py

--- Python - Exercise/Chapter-4/chap-4ex-5.py
+++ Python - Exercise/Chapter-4/chap-4ex-5.py
@@ -1,4 +1,12 @@
-# a program that combines lists and conditional statements
+"""
+Chapter 4, Exercise 5: Favorite fruit
+
+Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+•Make a list of your three favorite fruits and call it favorite_fruits.
+•Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block
+should print a statement,such as You really like bananas!
+"""
+
 
 fav_fruits = ['banana', 'mango', 'strawberry']
 
