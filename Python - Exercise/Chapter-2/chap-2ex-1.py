e7053c3302dbb5e6ab8e31f3cd1ff8e80ba0fea3
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-03 00:52:32 +0400

    Update chap-2ex-1.py

--- Python - Exercise/Chapter-2/chap-2ex-1.py
+++ Python - Exercise/Chapter-2/chap-2ex-1.py
@@ -1,8 +1,12 @@
-# a program to write a message in a variable, then printing it.
-# then changing the message of the same variable, and printing the new message.
+"""
+Chapter 2, Exercise 1: Variables
 
-message = 'Hi there, The python language is one of the easiest languages.'
+Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+"""
+
+message = 'Hi there, The python language is one of the easiest languages.'  # the first msg
 print(message)
 
-message = 'It is also the most popular programming language.'
+message = 'It is also the most popular programming language.' # the second msg
 print(message)
