6ba8bfc810581131f77da9f87d0e8bfe7cc494c8
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-04 19:06:28 +0400

    Update chap-1ex-3.py

--- Python - Exercise/Chapter-1/chap-1ex-3.py
+++ Python - Exercise/Chapter-1/chap-1ex-3.py
@@ -10,11 +10,13 @@ import time  # importing time module
 from datetime import date  # from the datetime library, importing date module
 from datetime import datetime # from datetime library, importing datetime module
 
+# printing date only
 date = date.today()
 print("Today's date is ", date)
 
 print("\n")
 
+# printing time only
 t = time.localtime()
 current_time = time.strftime("%H : %M : %S", t)  # date format has been defined between the brackets
 print('The current time is ', current_time)
