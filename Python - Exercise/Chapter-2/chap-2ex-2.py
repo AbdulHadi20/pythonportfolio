70305acfb1f7f8e1507e913112369b56c7d1d9b4
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2022-11-04 19:09:23 +0400

    Update chap-2ex-2.py

--- Python - Exercise/Chapter-2/chap-2ex-2.py
+++ Python - Exercise/Chapter-2/chap-2ex-2.py
@@ -5,7 +5,8 @@ Find a quote from a famous person you admire. Print the quote and the name of it
 Your output should look something like the following, including the quotation marks:
 Albert Einstein once said, “A person who never made a mistake never tried anything new.
 """
-
+# saving my favorite quote in a variable named quote
 quote = '"The best time to plant a tree was yesterday, the second best time is now."'
 
+# printing the quote using print() statement
 print('Ali Abdaal once quoted a famous proverb. He said, ' + str(quote))
